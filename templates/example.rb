eb135534cdf340a169ee34d8dd81d1375916a544
Yoichi Dan <dany1468@gmail.com>
2017-03-22 00:04:53 +0900

    Tidy example.rb

--- templates/example.rb
+++ templates/example.rb
@@ -5,29 +5,12 @@
 #   puts 'successfly connected'
 # end
 #
-#
 # # called when match message
 # hear 'foo' do
 #   puts 'foo'
 # end
 #
-#
-# # use regexp in string literal
-# hear 'bar (.*)' do |event|
-#   puts event.matches[1] #=> Event#matches return MatchData object
-# end
-#
-#
-# # event object is slack event JSON (convert to Hashie::Mash)
-# hear '^bar (.*)' do |event|
-#   puts event.channel #=> channel id
-#   say 'remoppy!', channel: event.channel #=> to received message channel
-#   say 'remoppy!', channel: '#general'
-#   say 'remoppy!', username: 'remoppy!', icon_emoji: ':remoppy:'
-# end
-#
-#
-# # use regexp literal
-# hear /^foobar/ do
-#   say 'remoppppy!'
+# hear '^hello, remoppy!' do |event|
+#   logger.info 'received message!'
+#   say 'hello!!', participation_id: event.participation
 # end
